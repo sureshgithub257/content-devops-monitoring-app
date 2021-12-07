4fa2659330b9b7a7b92bfd73e24e7df563817e76
sureshgithub257 <sureshgk257@gmail.com>
2021-12-07 09:57:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
 // Require and call Express
 var express = require('express');
 var bodyParser = require('body-parser');
+var responseTime = require('response-time');
 var app = express();
+
+const prom = require('prom-client');
+//Collect Default Metrics
+const collectDefaultMetrics = prom.collectDefaultMetrics;
+collectDefaultMetrics({ prifix: forethought});
+
+//Metrics list
+
+const todocounter = new prom.Counter({
+  name: 'forethought_number_of_new_todos',
+  help: 'The number of new tasks added to our application'
+});
+
+const todogauge = new prom.Gauge({
+  name: 'forethought_current_todos',
+  help: 'Amount of current incomplete tasks'
+});
+
+const tasksumm = new prom.Summary({
+  name: 'forethought_requests_summ',
+  help: 'Latency in percentiles'
+});
+
+const taskhisto = new prom.Histogram({
+  name: 'forethought_requests_histo',
+  help: 'Latency in histogram form',
+  buckets: [0.1, 0.25, 0.5, 1, 2.5, 5, 10]
+});
+
+//Add metrics endpoint
+app.get('/metrics', function (req, res) {
+  res.set('Content-Type', prom.register.contentType); 
+  res.end(prom.register.metrics());
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
@@ -18,6 +53,8 @@ app.post("/addtask", function(req, res) {
   var newTask = req.body.newtask;
   task.push(newTask);
   res.redirect("/");
+  todocounter.inc();
+  todogauge.inc();
 });
 
 // remove a task
@@ -31,10 +68,17 @@ app.post("/removetask", function(req, res) {
     for (var i = 0; i < completeTask.length; i++) {
       complete.push(completeTask[i]);
       task.splice(task.indexOf(completeTask[i]), 1);
+      todogauge.dec();
     }
   }
   res.redirect("/");
 });
+
+// Track response time
+app.use(responseTime(function(req, res, time){
+  tasksumm.observe(time);
+  taskhisto.observe(time);
+}))
 
 // get website files
 app.get("/", function (req, res) {
